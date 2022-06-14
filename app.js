88681f3894d08b6db4e2c182d62b5a5b69176a1e
Александр Парфенов <7081275@gmail.com>
2022-06-14 09:32:29 +0300

    1 commit

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
 });
 app.use('/', cards);
 app.use('/', users);
+app.use('*', (req, res) => res.status(404).send({message: "Страница не найдена"}));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
